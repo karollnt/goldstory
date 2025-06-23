8f61b314ba1806c301b38e0d361d88e40f63ccab
Karoll Navas <futlaos@gmail.com>
2025-06-23 15:52:28 -0500

    Update metamask-connect.js

--- es/metamask-connect.js
+++ es/metamask-connect.js
@@ -1,23 +1,23 @@
-import { MetaMaskSDK } from 'https://unpkg.com/@metamask/sdk@1.1.0/dist/browser/es.js';
+window.addEventListener("load", () => {
+  const MMSDK = new window.MetaMaskSDK.MetaMaskSDK({
+    dappMetadata: {
+      name: "Tu DApp Web",
+      url: window.location.href,
+    },
+    injectProvider: true,
+    preferDesktop: false,
+    headless: false,
+  });
 
-const MMSDK = new MetaMaskSDK({
-  dappMetadata: {
-    name: "Tu DApp Web",
-    url: window.location.href,
-  },
-  injectProvider: true,
-  preferDesktop: false,
-  headless: false,
+  const ethereum = MMSDK.getProvider();
+
+  document.getElementById("connectBtn").onclick = async () => {
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      document.getElementById("wallet").innerText = "Conectado: " + accounts[0];
+    } catch (err) {
+      console.error("Error al conectar", err);
+      document.getElementById("wallet").innerText = "Error al conectar wallet";
+    }
+  };
 });
-
-const ethereum = MMSDK.getProvider();
-
-document.getElementById("connectBtn").onclick = async () => {
-  try {
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    document.getElementById("wallet").innerText = "Conectado: " + accounts[0];
-  } catch (err) {
-    console.error("Error al conectar", err);
-    document.getElementById("wallet").innerText = "Error al conectar wallet";
-  }
-};
